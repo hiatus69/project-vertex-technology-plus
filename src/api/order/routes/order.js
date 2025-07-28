069c916819bcbb141c7fd02d1c38a79ccdb8a3a2
hiatus69 <narathip.fluk@gmail.com>
2025-07-28 23:29:15 +0700

    Update order.js

--- src/api/order/routes/order.js
+++ src/api/order/routes/order.js
@@ -6,4 +6,28 @@
 
 const { createCoreRouter } = require('@strapi/strapi').factories;
 
-module.exports = createCoreRouter('api::order.order');
+// เราจะ "ขยาย" router ที่มีอยู่เดิม
+const defaultRouter = createCoreRouter('api::order.order');
+
+const customRouter = (innerRouter, extraRoutes = []) => {
+  let routes;
+  return {
+    get prefix() {
+      return innerRouter.prefix;
+    },
+    get routes() {
+      if (!routes) routes = innerRouter.routes.concat(extraRoutes);
+      return routes;
+    },
+  };
+};
+
+const myExtraRoutes = [
+  {
+    method: 'GET',
+    path: '/orders/me',
+    handler: 'order.me', // <-- เปลี่ยน handler ให้ชี้ไปที่ controller 'order' และ action 'me'
+  },
+];
+
+module.exports = customRouter(defaultRouter, myExtraRoutes);
