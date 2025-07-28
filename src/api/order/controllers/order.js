69f7559ccba11345dc65b942442d9d3e94cab52d
hiatus69 <narathip.fluk@gmail.com>
2025-07-28 23:29:10 +0700

    Update order.js

--- src/api/order/controllers/order.js
+++ src/api/order/controllers/order.js
@@ -6,4 +6,32 @@
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::order.order');
+// เราจะ "ขยาย" controller ที่มีอยู่เดิม
+module.exports = createCoreController('api::order.order', ({ strapi }) => ({
+  
+  // นี่คือฟังก์ชัน "me" ของเราที่ถูกเพิ่มเข้ามาอย่างถูกต้อง
+  async me(ctx) {
+    const user = ctx.state.user;
+
+    if (!user) {
+      return ctx.unauthorized('You must be logged in.');
+    }
+
+    try {
+      const orders = await strapi.entityService.findMany('api::order.order', {
+        filters: {
+          customer: {
+            id: user.id,
+          },
+        },
+        populate: { order_items: true, completionProof: true },
+        sort: { createdAt: 'desc' },
+      });
+
+      return this.transformResponse(orders);
+    } catch (err) {
+      ctx.body = err;
+    }
+  },
+
+}));
