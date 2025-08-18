d8ed3066bffa89a84645b5ef984cad9eb034bb8f
hiatus69 <narathip.fluk@gmail.com>
2025-08-19 05:48:39 +0700

    Update order.js

--- src/api/order/routes/order.js
+++ src/api/order/routes/order.js
@@ -1,41 +1,11 @@
 // File: src/api/order/routes/order.js
 'use strict';
 
-module.exports = {
-  routes: [
-    {
-  method: 'GET',
-  path: '/orders/me',
-  handler: 'order.me',
-  config: {
-    policies: [], // <--- เช็กให้แน่ใจว่าเป็น array ว่างๆ แบบนี้
-  },
-},
-    // เราต้องนิยาม Route พื้นฐานใหม่ด้วย เพราะเรากำลังจะใช้ไฟล์นี้แทนที่ไฟล์อัตโนมัติ
-    {
-      method: 'GET',
-      path: '/orders',
-      handler: 'order.find',
-    },
-    {
-      method: 'GET',
-      path: '/orders/:id',
-      handler: 'order.findOne',
-    },
-    {
-      method: 'POST',
-      path: '/orders',
-      handler: 'order.create', // บอกให้วิ่งไปที่ฟังก์ชัน create ที่เราเขียนเอง
-    },
-    {
-      method: 'PUT',
-      path: '/orders/:id',
-      handler: 'order.update',
-    },
-    {
-      method: 'DELETE',
-      path: '/orders/:id',
-      handler: 'order.delete',
-    },
-  ],
-};
+/**
+ * order router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+// เราจะใช้ Core Router ตามปกติ และให้มันไปเรียกใช้ custom controller ของเราเอง
+module.exports = createCoreRouter('api::order.order');
